4ab7302450a1185431e1b97ba579a97e372c8b5c
Weslley Campos <weslley_campos@outlook.com>
2024-07-29 21:19:46 -0300

    ✨ feat: Criado o projeto base para macos.

---
+++ macos/yenom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  yenom
+//
+//  Created by Weslley Campos on 29/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ macos/yenom/yenomApp.swift
@@ -0,0 +1,17 @@
+//
+//  yenomApp.swift
+//  yenom
+//
+//  Created by Weslley Campos on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct yenomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
