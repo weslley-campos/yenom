e7589f0e02957ae6403a855d872639a9cc2c28bb
Weslley Campos <weslley_campos@outlook.com>
2024-08-02 19:54:35 -0300

    ✨feat: Mapeado as cores inicais para o arquivo de tema.

---
+++ macos/yenom/Theme/Theme.swift
@@ -0,0 +1,38 @@
+import Foundation
+import SwiftUI
+
+struct Theme {
+    // MARK: Colors
+    static let primary = Color("Primary")
+    static let primaryDarker = Color("PrimaryDarker")
+    static let primaryLighter = Color("PrimaryLighter")
+   
+    static let secondary = Color("Secondary")
+    static let secondaryDarker = Color("SecondaryDarker")
+    static let secondaryLighter = Color("SecondaryLighter")
+    
+    static let background = Color("Background")
+    static let onBackground = Color("OnBackground")
+    
+    static let surface = Color("Surface")
+    static let onSurface = Color("OnSurface")
+    
+    static let outline = Color("Outline")
+    static let separator = Color("Separator")
+    
+    static let error = Color("Error")
+    static let errorDarker = Color("ErrorDarker")
+    static let errorLighter = Color("ErrorLighter")
+    
+    static let warning = Color("Warning")
+    static let warningDarker = Color("WarningDarker")
+    static let warningLighter = Color("WarningLighter")
+    
+    static let info = Color("Info")
+    static let infoDarker = Color("InfoDarker")
+    static let infoLighter = Color("InfoLighter")
+    
+    static let success = Color("Success")
+    static let successDarker = Color("SuccessDarker")
+    static let successLighter = Color("SuccessLighter")
+}
